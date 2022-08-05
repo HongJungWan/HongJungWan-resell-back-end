3f97f7210349c5b88b2c61ed6180e78cfb56567c
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-05 17:58:08 +0900

    [doce] init

---
+++ src/test/java/com/resell/resell/ResellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resell.resell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
