a7f2abfd78f55f30d480b4872371b9a3b49cf158
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-10 17:54:47 +0900

    [feat] UserRepository
    findByEmail
    existsByEmail
    existsByNickname

---
+++ src/main/java/com/resell/resell/domain/users/user/UserRepository.java
@@ -0,0 +1,17 @@
+package com.resell.resell.domain.users.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 유저 이메일 찾기
+    Optional<User> findByEmail(String email);
+
+    // 이메일 중복 체크
+    boolean existsByEmail(String email);
+
+    // 닉네임 중복 체크
+    boolean existsByNickname(String nickname);
+}
