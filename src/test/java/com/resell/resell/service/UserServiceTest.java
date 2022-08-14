7e0a65169198c1fbe18b67432217b22de590c73d
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-14 14:43:51 +0900

    [test] UserServiceTest
    checkEmailDuplicate,
    checkNicknameDuplicate

--- src/test/java/com/resell/resell/service/UserServiceTest.java
+++ src/test/java/com/resell/resell/service/UserServiceTest.java
@@ -1,8 +1,20 @@
 package com.resell.resell.service;
 
+import com.resell.resell.controller.dto.UserDto;
+import com.resell.resell.domain.users.user.User;
+import com.resell.resell.domain.users.user.UserRepository;
+import com.resell.resell.exception.user.DuplicateEmailException;
+import com.resell.resell.exception.user.DuplicateNicknameException;
+import com.resell.resell.service.encrytion.EncryptionService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 /**
  * @ExtendWith : Junit5의 확장 어노테이션을 사용할 수 있다.
@@ -13,11 +25,50 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-    @Test
-    void checkEmailDuplicate() {
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    EncryptionService encryptionService;
+
+    @InjectMocks
+    UserService userService;
+
+    private UserDto.SaveRequest createUserDto() {
+        UserDto.SaveRequest saveRequest = UserDto.SaveRequest.builder()
+                .email("HongJungWan@test.com")
+                .password("test1234")
+                .phone("01077777777")
+                .nickname("GodHong")
+                .build();
+
+        return saveRequest;
+    }
+
+    private User createUser() {
+        return createUserDto().toEntity();
     }
 
     @Test
-    void checkNicknameDuplicate() {
+    @DisplayName("이메일 중복 -> 회원 가입 실패")
+    void checkEmailDuplicate() {
+        UserDto.SaveRequest saveRequest = createUserDto();
+        when(userRepository.existsByEmail("HongJungWan@test.com")).thenReturn(true);
+
+        assertThrows(DuplicateEmailException.class, () -> userService.save(saveRequest));
+
+        verify(userRepository, atLeastOnce()).existsByEmail("HongJungWan@test.com");
     }
+
+    @Test
+    @DisplayName("닉네임 중복 -> 회원 가입 실패")
+    void checkNicknameDuplicate() {
+        UserDto.SaveRequest saveRequest = createUserDto();
+        when(userRepository.existsByNickname("GodHong")).thenReturn(true);
+
+        assertThrows(DuplicateNicknameException.class, () -> userService.save(saveRequest));
+
+        verify(userRepository, atLeastOnce()).existsByNickname("GodHong");
+    }
+
 }
