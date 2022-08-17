46266478f3077395eb5bb50f338ee1b3d6106977
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-17 16:55:03 +0900

    [feat] SessionLoginService

---
+++ src/main/java/com/resell/resell/service/SessionLoginService.java
@@ -0,0 +1,52 @@
+package com.resell.resell.service;
+
+import com.resell.resell.domain.users.user.User;
+import com.resell.resell.domain.users.user.UserRepository;
+import com.resell.resell.exception.user.UserNotFoundException;
+import com.resell.resell.service.encrytion.EncryptionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpSession;
+
+import static com.resell.resell.common.utils.constants.UserConstants.AUTH_STATUS;
+import static com.resell.resell.common.utils.constants.UserConstants.USER_ID;
+import static com.resell.resell.controller.dto.UserDto.LoginRequest;
+
+@Service
+@RequiredArgsConstructor
+public class SessionLoginService {
+
+    private final HttpSession session;
+
+    private final UserRepository userRepository;
+
+    private final EncryptionService encryptionService;
+
+    @Transactional(readOnly = true)
+    public void existByEmailAndPassword(LoginRequest loginRequest) {
+        loginRequest.passwordEncryption(encryptionService);
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        if (!userRepository.existsByEmailAndPassword(email, password)) {
+            throw new UserNotFoundException("아이디 또는 비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public void login(LoginRequest loginRequest) {
+        existByEmailAndPassword(loginRequest);
+        String email = loginRequest.getEmail();
+        setUserLevel(email);
+        session.setAttribute(USER_ID, email);
+    }
+
+    public void setUserLevel(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("존재하지 않는 사용자 입니다."));
+
+        session.setAttribute(AUTH_STATUS, user.getUserLevel());
+    }
+
+}
