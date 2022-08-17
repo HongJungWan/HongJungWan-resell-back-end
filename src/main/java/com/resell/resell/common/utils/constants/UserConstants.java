23743459ac2de7ef4d2ed8ad4de8ab1200bdd3ba
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-17 16:54:09 +0900

    [feat] UserConstants

---
+++ src/main/java/com/resell/resell/common/utils/constants/UserConstants.java
@@ -0,0 +1,8 @@
+package com.resell.resell.common.utils.constants;
+
+public class UserConstants {
+
+    public static final String USER_ID = "email";
+    
+    public static final String AUTH_STATUS = "auth";
+}
