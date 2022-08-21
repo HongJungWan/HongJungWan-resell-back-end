5eff786a59bb41d4ca7c17b395bfffad1cc3efd2
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-21 13:18:32 +0900

    feat: EmailConstants

---
+++ src/main/java/com/resell/resell/common/utils/certification/email/EmailConstants.java
@@ -0,0 +1,9 @@
+package com.resell.resell.common.utils.certification.email;
+
+public class EmailConstants {
+    public static final String PREFIX_VERIFICATION = "email verification:";
+    public static final String TITLE_CERTIFICATION = "ReSell 인증번호 안내";
+    public static final String TITLE_EMAIL_CHECK = "[ReSell] 이메일 인증을 완료해 주세요";
+    public static final int LIMIT_TIME_EMAIL_VALIDATION = 1440 * 60;
+    public static final String DOMAIN_NAME = "http://localhost:8080";
+}
