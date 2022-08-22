fc084a902d7da136b17ecf8e9ef4f4489be58cc0
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-22 13:25:15 +0900

    feat: EmailCertificationDao

---
+++ src/main/java/com/resell/resell/dao/EmailCertificationDao.java
@@ -0,0 +1,12 @@
+package com.resell.resell.dao;
+
+public interface EmailCertificationDao {
+
+    void createEmail(String email, String certificationNumber);
+
+    String getEmailCertification(String email);
+
+    void removeEmailCertification(String email);
+
+    boolean hasKey(String email);
+}
