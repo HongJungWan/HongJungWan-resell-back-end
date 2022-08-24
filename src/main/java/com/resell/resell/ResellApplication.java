99ac0a5c8a69390b15d83a02c6289bc0e27b5aa9
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-24 14:56:47 +0900

    feat(ResellApplication): @EnableConfigurationProperties

--- src/main/java/com/resell/resell/ResellApplication.java
+++ src/main/java/com/resell/resell/ResellApplication.java
@@ -1,13 +1,16 @@
 package com.resell.resell;
 
+import com.resell.resell.common.utils.properties.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {AppProperties.class})
 public class ResellApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResellApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResellApplication.class, args);
+    }
 
 }
