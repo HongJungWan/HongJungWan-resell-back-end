78564ed345b90d4a83983efe98e03dde27cc669e
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-24 14:57:43 +0900

    feat : User
    updateUserLevel,
    toUserInfoDto,
    toFindUserDto,
    updatePassword

--- src/main/java/com/resell/resell/domain/users/user/User.java
+++ src/main/java/com/resell/resell/domain/users/user/User.java
@@ -1,5 +1,6 @@
 package com.resell.resell.domain.users.user;
 
+import com.resell.resell.controller.dto.UserDto.FindUserResponse;
 import com.resell.resell.domain.users.common.Account;
 import com.resell.resell.domain.users.common.UserBase;
 import com.resell.resell.domain.users.common.UserLevel;
@@ -10,6 +11,8 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
+import static com.resell.resell.controller.dto.UserDto.UserInfoDto;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,17 +20,37 @@ import java.time.LocalDateTime;
 public class User extends UserBase {
 
     private String nickname;
-
     private String phone;
 
     @Embedded // 값 타입을 사용하는 곳에 표시
     private Account account;
-
     private Long point;
-
     private LocalDateTime nicknameModifiedDate;
-
     private UserStatus userStatus;
+
+    public void updateUserLevel() {
+        this.userLevel = UserLevel.AUTH;
+    }
+
+    public UserInfoDto toUserInfoDto() {
+        return UserInfoDto.builder()
+                .email(this.getEmail())
+                .nickname(this.getNickname())
+                .phone(this.getPhone())
+                .userLevel(this.userLevel)
+                .build();
+    }
+
+    public FindUserResponse toFindUserDto() {
+        return FindUserResponse.builder()
+                .email(this.getEmail())
+                .phone(this.getPhone())
+                .build();
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
 
     @Builder
     public User(Long id, String email, String password, UserLevel userLevel, String nickname, String phone, LocalDateTime nicknameModifiedDate, UserStatus userStatus, Long point) {
