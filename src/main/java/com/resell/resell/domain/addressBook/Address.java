be35f04757adfacfc573bfc046d68258c1291667
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-26 01:21:12 +0900

    feat(domain) : Address

---
+++ src/main/java/com/resell/resell/domain/addressBook/Address.java
@@ -0,0 +1,40 @@
+package com.resell.resell.domain.addressBook;
+
+import com.resell.resell.controller.dto.AddressDto;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String addressName;
+    private String roadNameAddress;
+    private String detailedAddress;
+    private String postalCode;
+
+    public void updateAddress(AddressDto.SaveRequest requestDto) {
+        this.addressName = requestDto.getAddressName();
+        this.roadNameAddress = requestDto.getRoadNameAddress();
+        this.detailedAddress = requestDto.getDetailedAddress();
+        this.postalCode = requestDto.getPostalCode();
+    }
+
+    @Builder
+    public Address(String addressName, String roadNameAddress, String detailedAddress,
+                   String postalCode) {
+        this.addressName = addressName;
+        this.roadNameAddress = roadNameAddress;
+        this.detailedAddress = detailedAddress;
+        this.postalCode = postalCode;
+    }
+}
