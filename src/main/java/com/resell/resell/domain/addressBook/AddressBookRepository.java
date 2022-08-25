b3f27f7258ce94313d7e464ff705206855f42383
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-26 01:22:41 +0900

    feat(domain) : AddressBookRepository

---
+++ src/main/java/com/resell/resell/domain/addressBook/AddressBookRepository.java
@@ -0,0 +1,6 @@
+package com.resell.resell.domain.addressBook;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressBookRepository extends JpaRepository<AddressBook, Long> {
+}
