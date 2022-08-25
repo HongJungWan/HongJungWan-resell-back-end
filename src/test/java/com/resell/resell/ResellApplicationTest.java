b1caf2e321415d77202f60696945069c0761d080
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-26 01:24:26 +0900

    chore : ResellApplicationTest

---
+++ src/test/java/com/resell/resell/ResellApplicationTest.java
@@ -0,0 +1,12 @@
+package com.resell.resell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
