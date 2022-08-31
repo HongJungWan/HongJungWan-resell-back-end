c7e21636c3373ac2369a792eaeb4cf9b74225001
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-08-31 14:44:44 +0900

    feat : UserServiceTest
    updatePasswordByForget,
    SuccessToGetUserResource,
    failToGetUserResource,
    updatePassword,

--- src/test/java/com/resell/resell/service/UserServiceTest.java
+++ src/test/java/com/resell/resell/service/UserServiceTest.java
@@ -1,10 +1,11 @@
 package com.resell.resell.service;
 
-import com.resell.resell.controller.dto.UserDto;
+import com.resell.resell.domain.addressBook.AddressRepository;
 import com.resell.resell.domain.users.user.User;
 import com.resell.resell.domain.users.user.UserRepository;
 import com.resell.resell.exception.user.DuplicateEmailException;
 import com.resell.resell.exception.user.DuplicateNicknameException;
+import com.resell.resell.exception.user.UserNotFoundException;
 import com.resell.resell.service.encrytion.EncryptionService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static com.resell.resell.controller.dto.UserDto.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -31,11 +36,14 @@ class UserServiceTest {
     @Mock
     EncryptionService encryptionService;
 
+    @Mock
+    AddressRepository addressRepository;
+
     @InjectMocks
     UserService userService;
 
-    private UserDto.SaveRequest createUserDto() {
-        UserDto.SaveRequest saveRequest = UserDto.SaveRequest.builder()
+    private SaveRequest createUserDto() {
+        SaveRequest saveRequest = SaveRequest.builder()
                 .email("HongJungWan@test.com")
                 .password("test1234")
                 .phone("01077777777")
@@ -52,7 +60,7 @@ class UserServiceTest {
     @Test
     @DisplayName("이메일 중복 -> 회원 가입 실패")
     void checkEmailDuplicate() {
-        UserDto.SaveRequest saveRequest = createUserDto();
+        SaveRequest saveRequest = createUserDto();
         when(userRepository.existsByEmail("HongJungWan@test.com")).thenReturn(true);
 
         assertThrows(DuplicateEmailException.class, () -> userService.save(saveRequest));
@@ -63,12 +71,85 @@ class UserServiceTest {
     @Test
     @DisplayName("닉네임 중복 -> 회원 가입 실패")
     void checkNicknameDuplicate() {
-        UserDto.SaveRequest saveRequest = createUserDto();
+        SaveRequest saveRequest = createUserDto();
         when(userRepository.existsByNickname("GodHong")).thenReturn(true);
 
         assertThrows(DuplicateNicknameException.class, () -> userService.save(saveRequest));
 
         verify(userRepository, atLeastOnce()).existsByNickname("GodHong");
+    }
+
+    @Test
+    @DisplayName("비밀번호 찾기 -> 전달받은 객체(이메일)가 회원이면 -> 비밀번호 변경 성공")
+    public void updatePasswordByForget() {
+        ChangePasswordRequest changePasswordRequest = ChangePasswordRequest.builder()
+                .email("HongJungWan@test.com")
+                .passwordAfter("test12345")
+                .build();
+        User user = createUserDto().toEntity();
+
+        when(userRepository.findByEmail(changePasswordRequest.getEmail()))
+                .thenReturn(Optional.of(user));
+
+        userService.updatePasswordByForget(changePasswordRequest);
+
+        assertThat(user.getPassword()).isEqualTo(changePasswordRequest.getPasswordAfter());
+
+        verify(userRepository, atLeastOnce()).findByEmail(changePasswordRequest.getEmail());
+    }
+
+    @Test
+    @DisplayName("[가입 O] 이메일 입력 -> 비밀번호 변경에 필요한 리소스 리턴")
+    public void SuccessToGetUserResource() {
+        String email = "HongJungWan@test.com";
+        User user = createUserDto().toEntity();
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+
+        FindUserResponse userResource = userService.getUserResource(email);
+
+        assertThat(userResource.getEmail()).isEqualTo(user.getEmail());
+        assertThat(userResource.getPhone()).isEqualTo(user.getPhone());
+    }
+
+    @Test
+    @DisplayName("[가입 X] 이메일 입력 -> 비밀번호 변경에 필요한 리소스 리턴 실패")
+    public void failToGetUserResource() {
+
+        String email = "not@test.com";
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class,
+                () -> userService.getUserResource("not@test.com"));
+
+        verify(userRepository, atLeastOnce()).findByEmail(email);
+    }
+
+    @Test
+    @DisplayName("비밀번호 변경 -> 이전 비밀번호와 일치 시 -> 비밀번호 변경 성공")
+    public void updatePassword() {
+        User user = createUserDto().toEntity();
+        ChangePasswordRequest changePasswordRequest = ChangePasswordRequest
+                .builder()
+                .email(null)
+                .passwordBefore("test1234")
+                .passwordAfter("test12345")
+                .build();
+
+        String email = "HongJungWan@test.com";
+        String passwordBefore = encryptionService.encrypt(changePasswordRequest.getPasswordBefore());
+        String passwordAfter = encryptionService.encrypt(changePasswordRequest.getPasswordAfter());
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        when(userRepository.existsByEmailAndPassword(email, passwordBefore)).thenReturn(true);
+
+        userService.updatePassword(email, changePasswordRequest);
+
+        assertThat(user.getPassword()).isEqualTo(passwordAfter);
+
+        verify(userRepository, atLeastOnce()).findByEmail(email);
+        verify(userRepository, atLeastOnce()).existsByEmailAndPassword(email, passwordBefore);
     }
 
 }
