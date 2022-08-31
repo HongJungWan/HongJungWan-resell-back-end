6ba6232ee1c9237fd8414d99f88a4db436c9e581
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-09-01 06:28:47 +0900

    feat : UserApiController

--- src/main/java/com/resell/resell/controller/UserApiController.java
+++ src/main/java/com/resell/resell/controller/UserApiController.java
@@ -2,6 +2,8 @@ package com.resell.resell.controller;
 
 import com.resell.resell.common.utils.annotation.CurrentUser;
 import com.resell.resell.common.utils.annotation.LoginCheck;
+import com.resell.resell.controller.dto.UserDto;
+import com.resell.resell.service.EmailCertificationService.EmailCertificationService;
 import com.resell.resell.service.SessionLoginService;
 import com.resell.resell.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 import static com.resell.resell.common.utils.constants.ResponseConstants.CREATED;
-import static com.resell.resell.controller.dto.UserDto.LoginRequest;
-import static com.resell.resell.controller.dto.UserDto.SaveRequest;
+import static com.resell.resell.controller.dto.UserDto.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -20,8 +21,8 @@ import static com.resell.resell.controller.dto.UserDto.SaveRequest;
 public class UserApiController {
 
     private final UserService userService;
-
     private final SessionLoginService sessionLoginService;
+    private final EmailCertificationService emailCertificationService;
 
     @GetMapping("/user-emails/{email}/exists")
     public ResponseEntity<Boolean> checkEmailDuplicate(@PathVariable String email) {
@@ -34,8 +35,10 @@ public class UserApiController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> createUser(@Valid @RequestBody SaveRequest requestDto) {
+    public ResponseEntity<Void> createUser(@Valid @RequestBody UserDto.SaveRequest requestDto) {
         userService.save(requestDto);
+        emailCertificationService.sendEmailForEmailCheck(requestDto.getEmail());
+
         return CREATED;
     }
 
@@ -55,4 +58,44 @@ public class UserApiController {
         userService.updateEmailVerified(token, email);
     }
 
+    @PostMapping("/resend-email-token")
+    public void resendEmailCheck(@CurrentUser String email) {
+        emailCertificationService.sendEmailForEmailCheck(email);
+    }
+
+    @GetMapping("/myInfo")
+    public ResponseEntity<UserInfoDto> myPage(@CurrentUser String email) {
+        UserInfoDto loginUser = sessionLoginService.getCurrentUser(email);
+        return ResponseEntity.ok(loginUser);
+    }
+
+    @GetMapping("/find/{email}")
+    public ResponseEntity<FindUserResponse> findUser(@PathVariable String email) {
+        FindUserResponse findUserResponse = userService.getUserResource(email);
+        return ResponseEntity.ok(findUserResponse);
+    }
+
+    @PostMapping("/email-certification/sends")
+    public ResponseEntity sendEmail(@RequestBody EmailCertificationRequest requestDto) {
+        emailCertificationService.sendEmailForCertification(requestDto.getEmail());
+        return CREATED;
+    }
+
+    @PostMapping("/email-certification/confirms")
+    public void emailVerification(@RequestBody EmailCertificationRequest requestDto) {
+        emailCertificationService.verifyEmail(requestDto);
+    }
+
+    @PatchMapping("/forget/password")
+    public void changePasswordByForget(
+            @Valid @RequestBody ChangePasswordRequest requestDto) {
+        userService.updatePasswordByForget(requestDto);
+    }
+
+    @LoginCheck
+    @PatchMapping("/password")
+    public void changePassword(@CurrentUser String email, @Valid @RequestBody ChangePasswordRequest requestDto) {
+        userService.updatePassword(email, requestDto);
+    }
+    
 }
