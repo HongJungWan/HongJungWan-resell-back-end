57928c20c0f7884ed09e868b410f2b1a0f31e7ba
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-09-06 13:28:29 +0900

    feat : ProductRepository

---
+++ src/main/java/com/resell/resell/domain/product/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.resell.resell.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    boolean existsByModelNumber(String modelNumber);
+
+}
