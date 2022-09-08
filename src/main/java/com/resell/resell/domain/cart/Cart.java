945153b839415ada4e5a8d6f14ac8529400c59b0
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-09-09 00:14:51 +0900

    feat : Cart

---
+++ src/main/java/com/resell/resell/domain/cart/Cart.java
@@ -0,0 +1,25 @@
+package com.resell.resell.domain.cart;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @OneToMany(mappedBy = "cart", orphanRemoval = true)
+    private Set<CartProduct> wishList = new HashSet<>();
+
+}
