efa936e0363de93592094a73e74834e77db33f9a
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-09-09 00:16:22 +0900

    feat : ProductApiController

---
+++ src/main/java/com/resell/resell/controller/ProductApiController.java
@@ -0,0 +1,28 @@
+package com.resell.resell.controller;
+
+import com.resell.resell.common.utils.annotation.LoginCheck;
+import com.resell.resell.domain.users.common.UserLevel;
+import com.resell.resell.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.resell.resell.controller.dto.ProductDto.SaveRequest;
+
+@RequiredArgsConstructor
+@RequestMapping("/products")
+@RestController
+public class ProductApiController {
+
+    private final ProductService productService;
+
+    @LoginCheck(authority = UserLevel.ADMIN)
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createProduct(@Valid @RequestParam SaveRequest requestDto) {
+        productService.saveProduct(requestDto);
+    }
+
+}
