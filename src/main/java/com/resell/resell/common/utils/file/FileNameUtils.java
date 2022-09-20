8fbcaddb8ad9b2e3fd8fa1d710bbbb3d195ad593
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-09-20 19:51:47 +0900

    feat : FileNameUtils

---
+++ src/main/java/com/resell/resell/common/utils/file/FileNameUtils.java
@@ -0,0 +1,36 @@
+package com.resell.resell.common.utils.file;
+
+import com.resell.resell.exception.file.IllegalMimeTypeException;
+
+import java.util.UUID;
+
+public class FileNameUtils {
+
+    public static void checkImageMimeType(String mimeType) {
+        if (!(mimeType.equals("image/jpg") || mimeType.equals("image/jpeg")
+                || mimeType.equals("image/png") || mimeType.equals("image/gif"))) {
+            throw new IllegalMimeTypeException();
+        }
+    }
+
+    public static String fileNameConvert(String fileName) {
+        StringBuilder builder = new StringBuilder();
+        UUID uuid = UUID.randomUUID();
+        String extension = getExtension(fileName);
+
+        builder.append(uuid).append(".").append(extension);
+
+        return builder.toString();
+    }
+
+    private static String getExtension(String fileName) {
+        int pos = fileName.lastIndexOf(".");
+
+        return fileName.substring(pos + 1);
+    }
+
+    public static String toThumbnail(String src) {
+        return src.replaceFirst("origin", "thumbnail");
+    }
+
+}
