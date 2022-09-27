9a01aed8006e18914ee04905e125570c22d9c396
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-09-27 19:43:38 +0900

    feat : ResellApplication.java

--- src/main/java/com/resell/resell/ResellApplication.java
+++ src/main/java/com/resell/resell/ResellApplication.java
@@ -1,16 +1,16 @@
 package com.resell.resell;
 
-import com.resell.resell.common.utils.properties.AppProperties;
+import com.resell.resell.common.properties.AppProperties;
+import com.resell.resell.common.properties.AwsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = {AppProperties.class})
+@EnableConfigurationProperties(value = {AppProperties.class, AwsProperties.class})
 public class ResellApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ResellApplication.class, args);
     }
-
 }
