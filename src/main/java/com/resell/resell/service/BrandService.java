12729b7726e8d5a18e46d96d5020c2d83e2adcda
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-09-30 12:20:30 +0900

    feat : BrandService
    deleteBrand(), updateBrand(), isDeleteSavedImage(),
    checkDuplicateUpdatedNameKor(), checkDuplicateUpdatedNameEng

--- src/main/java/com/resell/resell/service/BrandService.java
+++ src/main/java/com/resell/resell/service/BrandService.java
@@ -5,6 +5,7 @@ import com.resell.resell.controller.dto.BrandDto;
 import com.resell.resell.controller.dto.BrandDto.SaveRequest;
 import com.resell.resell.domain.brand.Brand;
 import com.resell.resell.domain.brand.BrandRepository;
+import com.resell.resell.exception.brand.BrandNotFoundException;
 import com.resell.resell.exception.brand.DuplicateBrandNameException;
 import com.resell.resell.service.storage.AwsS3Service;
 import lombok.RequiredArgsConstructor;
@@ -50,6 +51,53 @@ public class BrandService {
                 .collect(Collectors.toList());
     }
 
+    /*
+     * 기존 이미지 삭제 조건
+     * 1) requestDto의 imagePath가 null이고, 저장된 brand의 imagePath가 null이 아니라면
+     * 2) requestDto의 imagePath가 null이 아니고, 요청에 MultipartFile이 존재한다면
+     * 스토리지에서 기존의 이미지를 delete하고, imagePath를 null로 초기화한다.
+     * (기존 이미지 삭제 조건을 철저히 하여 스토리지 내 유령 파일을 만들지 않도록 한다.)
+     */
+
+    @CacheEvict(value = "brands", allEntries = true)
+    @Transactional
+    public void deleteBrand(Long id) {
+        Brand brand = brandRepository.findById(id)
+                .orElseThrow(BrandNotFoundException::new);
+        String path = brand.getOriginImagePath();
+
+        brandRepository.deleteById(id);
+        if (path != null) {
+            String key = FileNameUtils.getFileName(path);
+            awsS3Service.deleteBrandImage(key);
+        }
+    }
+
+    @CacheEvict(value = "brands", allEntries = true)
+    @Transactional
+    public void updateBrand(Long id, SaveRequest updatedBrand, @Nullable MultipartFile brandImage) {
+        Brand savedBrand = brandRepository.findById(id)
+                .orElseThrow(() -> new BrandNotFoundException());
+        String savedImagePath = savedBrand.getOriginImagePath();
+        String updatedImagePath = updatedBrand.getOriginImagePath();
+
+        checkDuplicateUpdatedNameKor(savedBrand.getNameKor(), updatedBrand.getNameKor());
+        checkDuplicateUpdatedNameEng(savedBrand.getNameEng(), updatedBrand.getNameEng());
+
+        if (isDeleteSavedImage(savedImagePath, updatedImagePath, brandImage)) {
+            String key = FileNameUtils.getFileName(savedImagePath);
+            awsS3Service.deleteBrandImage(key);
+            updatedBrand.deleteImagePath();
+        }
+        if (brandImage != null) {
+            String originImagePath = awsS3Service.uploadBrandImage(brandImage);
+            String thumbnailImagePath = FileNameUtils.toThumbnail(originImagePath);
+            updatedBrand.setImagePath(originImagePath, thumbnailImagePath);
+        }
+
+        savedBrand.update(updatedBrand);
+    }
+
     private boolean checkDuplicateName(SaveRequest requestDto) {
         if (brandRepository.existsByNameKor(requestDto.getNameKor())) {
             return true;
@@ -57,6 +105,30 @@ public class BrandService {
             return true;
         }
         return false;
+    }
+
+    private boolean isDeleteSavedImage(String savedImagePath, String updatedImagePath,
+                                       MultipartFile brandImage) {
+        return ((updatedImagePath == null && savedImagePath != null) ||
+                (savedImagePath != null && brandImage != null));
+    }
+
+    private void checkDuplicateUpdatedNameKor(String nameKor, String updatedNameKor) {
+        if (nameKor.equals(updatedNameKor)) {
+            return;
+        } else if (!brandRepository.existsByNameKor(updatedNameKor)) {
+            return;
+        }
+        throw new DuplicateBrandNameException();
+    }
+
+    private void checkDuplicateUpdatedNameEng(String nameEng, String updatedNameEng) {
+        if (nameEng.equals(updatedNameEng)) {
+            return;
+        } else if (!brandRepository.existsByNameEng(updatedNameEng)) {
+            return;
+        }
+        throw new DuplicateBrandNameException();
     }
 
 }
