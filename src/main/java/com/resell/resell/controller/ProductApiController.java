b892e25013e63da8f88004e414c3278940f79bed
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-10-14 00:18:22 +0900

    feat : ProductApiController
    productService.saveProduct

--- src/main/java/com/resell/resell/controller/ProductApiController.java
+++ src/main/java/com/resell/resell/controller/ProductApiController.java
@@ -2,10 +2,12 @@ package com.resell.resell.controller;
 
 import com.resell.resell.common.annotation.LoginCheck;
 import com.resell.resell.domain.users.common.UserLevel;
+import com.resell.resell.service.BrandService;
 import com.resell.resell.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 
@@ -18,11 +20,13 @@ public class ProductApiController {
 
     private final ProductService productService;
 
+    private final BrandService brandService;
+
     @LoginCheck(authority = UserLevel.ADMIN)
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void createProduct(@Valid @RequestParam SaveRequest requestDto) {
-        productService.saveProduct(requestDto);
+    public void createProduct(@Valid @RequestPart SaveRequest requestDto, @RequestPart(required = false) MultipartFile productImage) {
+        brandService.checkBrandExist(requestDto.getBrand());
+        productService.saveProduct(requestDto, productImage);
     }
-
 }
