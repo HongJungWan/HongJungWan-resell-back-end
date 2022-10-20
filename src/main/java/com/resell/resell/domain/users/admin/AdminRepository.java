055b6ebf0e1ae7db97336c79f7e4c45a33f44a11
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-10-21 00:45:37 +0900

    feat : AdminRepository
    searchByUsers

--- src/main/java/com/resell/resell/domain/users/admin/AdminRepository.java
+++ src/main/java/com/resell/resell/domain/users/admin/AdminRepository.java
@@ -1,5 +1,10 @@
 package com.resell.resell.domain.users.admin;
 
+import com.resell.resell.controller.dto.UserDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface AdminRepository {
-    
+
+    Page<UserDto.UserListResponse> searchByUsers(UserDto.UserSearchCondition searchRequest, Pageable pageable);
 }
