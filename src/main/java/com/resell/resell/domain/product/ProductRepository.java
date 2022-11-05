cc317dd580713a961fc40cc4cf1b5faed7a60bdd
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-06 03:46:45 +0900

    feat : ProductRepository
    findById

--- src/main/java/com/resell/resell/domain/product/ProductRepository.java
+++ src/main/java/com/resell/resell/domain/product/ProductRepository.java
@@ -1,11 +1,18 @@
 package com.resell.resell.domain.product;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, SearchProductRepository {
 
     boolean existsByModelNumber(String modelNumber);
+
+    @Override
+    @EntityGraph(attributePaths = {"trades", "brand"})
+    Optional<Product> findById(Long id);
 
 }
