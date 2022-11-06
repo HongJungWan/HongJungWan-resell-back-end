b6d820040f05afebdddf1c8ce4017afc8e8fb45f
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-07 06:45:36 +0900

    feat : UserRepository
    findByEmail

--- src/main/java/com/resell/resell/domain/users/user/UserRepository.java
+++ src/main/java/com/resell/resell/domain/users/user/UserRepository.java
@@ -1,14 +1,12 @@
 package com.resell.resell.domain.users.user;
 
 import com.resell.resell.domain.users.admin.AdminRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, AdminRepository {
-
-    // 유저 이메일 찾기
-    Optional<User> findByEmail(String email);
 
     // 이메일 중복 체크
     boolean existsByEmail(String email);
@@ -18,4 +16,11 @@ public interface UserRepository extends JpaRepository<User, Long>, AdminReposito
 
     // 이메일, 비밀번호 불일치 체크
     boolean existsByEmailAndPassword(String email, String password);
+
+    void deleteByEmail(String email);
+
+    // 유저 이메일 찾기, 위시리스트용
+    @EntityGraph(attributePaths = {"addressBook", "cart"})
+    Optional<User> findByEmail(String email);
+
 }
