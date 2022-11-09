bdaca8efd4de492ea1396e906b42679c372b3e24
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-11-09 15:35:01 +0900

    feat : CartApiController
    getWishList(),
    deleteWishList()

--- src/main/java/com/resell/resell/controller/CartApiController.java
+++ src/main/java/com/resell/resell/controller/CartApiController.java
@@ -2,15 +2,14 @@ package com.resell.resell.controller;
 
 import com.resell.resell.common.annotation.CurrentUser;
 import com.resell.resell.common.annotation.LoginCheck;
-import com.resell.resell.controller.dto.AddressDto;
 import com.resell.resell.service.CartService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static com.resell.resell.controller.dto.AddressDto.*;
+import java.util.Set;
+
+import static com.resell.resell.controller.dto.ProductDto.*;
+import static com.resell.resell.controller.dto.ProductDto.IdRequest;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +18,23 @@ public class CartApiController {
 
     private final CartService cartService;
 
+    // 장바구니에 상품 담기
     @LoginCheck
     @PostMapping
     public void addWishList(@CurrentUser String email, @RequestBody IdRequest idRequest) {
         cartService.addWishList(email, idRequest);
+    }
+
+    @LoginCheck
+    @GetMapping
+    public Set<WishItemResponse> getWishList(@CurrentUser String email) {
+        return cartService.getWishList(email);
+    }
+
+    @LoginCheck
+    @DeleteMapping
+    public void deleteWishList(@RequestBody IdRequest idRequest) {
+        cartService.deleteWishList(idRequest);
     }
 
 }
